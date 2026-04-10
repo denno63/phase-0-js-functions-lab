a10a57b587fdfe95bcc046bd05daf4739cb93bc8
denno63 <mwaurakarobia2@gmail.com>
2026-04-10 10:55:28 +0300

    added index.js and adde code in it

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 
+function calculateTax(amount) {
+    return amount * 0.10;
+}
 
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+
+function isPalindrome(word) {
+    
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
+
+module.exports = {
+
+    calculateTax,
+    convertToUpperCase,
+    findMaximum,
+    isPalindrome,
+    calculateDiscountedPrice 
+
+}
